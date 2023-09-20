103c381ec2cedc778192d18a97385fbff3a01750
gary gritsch <garygritsch@gmail.com>
2023-09-20 14:56:57 -0700

    completed excersises js

---
+++ exercises/loop-olympics/index.js
@@ -0,0 +1,32 @@
+
+/*for (var i = 0; i <= 9; i++) {
+    console.log (i)
+} */
+
+/*for (var i = 9; i >= 0; i--) {
+    console.log(i)
+} */
+
+/*const fruit = ["banana", "orange", "apple", "kiwi"]
+for (var i = 0; i < fruit.length; i++) {
+    console.log ( fruit[i])
+} */
+
+/*var arrayNumbers = []
+for ( var i = 0; i <= 9; i++) {
+    arrayNumbers.push(i)
+}
+console.log (arrayNumbers) */
+
+/* for ( var i = 0; i <= 100; i++) {
+    if ( i % 2 === 0) {
+        console.log(i)
+    }
+} */
+
+const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+var food = []
+for ( var i = 0; i < fruit.length; i++) {
+    food.push(i)
+}
+console.log(fruit)
