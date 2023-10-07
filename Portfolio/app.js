2a25d4bd3f83aa509473a3ef3f812ad9d50548eb
gary gritsch <garygritsch@gmail.com>
2023-10-06 19:02:55 -0700

    added projects sign on road

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -9,7 +9,51 @@ var developer = {
         country: "united states"    
     }
 }
-const element = document.getElementById(container2)
-element.addEventListener("click", function() {
 
-})
+const cons = document.querySelectorAll(".con")
+const containers = document.querySelector(".containers")
+const shrink = (e) => {
+    const el = e.target
+  
+        // Remove cloned element from DOM after animation is over
+        el.addEventListener("animationend", (e) => e.target.remove())
+
+  // Trigger browser reflow to start animation
+        el.style.animation = 'none';
+        el.offsetHeight
+        el.style.animation = ''
+        el.classList.add("shrink-down")
+    }
+
+    const toggleFullScreen = (e) => {
+  // Get position values for element
+    const {
+        top,
+        left
+    } = e.target.getBoundingClientRect()
+
+    // Clone the element and its children
+    let fullScreen = e.target.cloneNode(true)
+
+    // Set top and left with custom property
+    fullScreen.style.setProperty("--inset", `${top}px auto auto ${left}px`)
+
+    // Add class with animation and position
+    fullScreen.classList.add("full-screen")
+
+    // Listen for click to close full screen
+    fullScreen.addEventListener("click", shrink)
+
+    // Place in container over element to expand
+    containers.appendChild(fullScreen)
+    }
+
+// Add click listeners on all boxes
+    cons.forEach(con => {
+        con.addEventListener("click", toggleFullScreen)
+    })
+    cons.forEach(con => {
+        con.addEventListener("click", (e) =>{
+            window.location.assign(URL = "https://www.reddit.com/r/css/comments/2v70is/can_anyone_help_trying_to_add_a_delay_between/");
+        },300);  
+    })
