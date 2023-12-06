a9d9446ea16f6ffc7a23b4fc5ab3555d7ec63add
gary gritsch <garygritsch@gmail.com>
2023-12-06 07:26:46 -0800

    completed code challenge

--- code-challenge/index.js
+++ code-challenge/index.js
@@ -7,3 +7,8 @@ console.log(names)
 
 
 
+const people = [ "John", "Adam", "Amber" ]
+
+const peopleElements = people.map(person => `<h1> ${person} </h1>`)
+
+console.log(peopleElements) //Expected Output: [ "<h1>John</h1>", "<h1>Adam</h1>", "<h1>Amber</h1>" ]
