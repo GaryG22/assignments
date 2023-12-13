dafc8cd8da811c7d185c1ddac12bc0c51e9aaca5
gary gritsch <garygritsch@gmail.com>
2023-12-13 06:33:56 -0800

    completed use state practice assignment

---
+++ useState-practice/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>colors</h1>
+    </div>
+  )
+}
+
+export default App
+
+//const [color, setColor] = useState("pink")
+//setColor("blue")
+
+
+//const [color, setColor] = useState("pink")
+
+/*setColor( prevColor => {
+  if (prevColor === "pink") {
+    prevColor = "blue"
+  } if (prevColor === "blue"){
+    prevColor = "pink"
+  }
+})*/
+
+
+
+/*const [people, setPeople] = useState([
+	{
+		firstName: "John",
+		lastName: "Smith"
+	},
+  {
+    firstName: "Aaron",
+    lastName: "Rodgers"
+  }
+])
+
+setPeople()*/
